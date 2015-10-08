b0fda4ef001cbcf63a18a3917a21cb2117511bf8
Mitch Currie <mitch@mitchellcurrie.com>
2015-10-09 10:45:27 +1100

    Initial Commit

---
+++ MCSwift/MCSwift.h
@@ -0,0 +1,19 @@
+//
+//  MCSwift.h
+//  MCSwift
+//
+//  Created by Mitchell Currie on 9/10/2015.
+//  Copyright © 2015 Mitch Currie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCSwift.
+FOUNDATION_EXPORT double MCSwiftVersionNumber;
+
+//! Project version string for MCSwift.
+FOUNDATION_EXPORT const unsigned char MCSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSwift/PublicHeader.h>
+
+
